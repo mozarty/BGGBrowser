b6bbdea2c3ce3519f6c95815a258d66e814cebea
Ahmed Salem <ahmed.salem@omio.com>
2020-12-01 04:48:12 +0100

    list of trending games added

--- app/src/main/java/com/salem/bggbrowser/view/ui/home/BoardGameMetadaViewHolder.kt
+++ app/src/main/java/com/salem/bggbrowser/view/ui/home/BoardGameMetadaViewHolder.kt
@@ -2,8 +2,8 @@ package com.salem.bggbrowser.view.ui.home
 
 import android.view.View
 
-import kotlinx.android.synthetic.main.list_item_metadata_layout.*
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.salem.bggbrowser.model.dto.BoardGameMetadataDto
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.list_item_metadata_layout.view.*
@@ -20,6 +20,9 @@ class BoardGameMetadaViewHolder constructor(itemView: View) : RecyclerView.ViewH
 
     fun bind(boardGameMetadataDto: BoardGameMetadataDto) {
         itemView.gameTitle.text = boardGameMetadataDto.name
+        Glide.with(itemView.context).load(boardGameMetadataDto.imageURL).centerCrop().into(itemView.thumbnail);
+        itemView.rank.text =  "# ${boardGameMetadataDto.rank}"
+        itemView.year.text = boardGameMetadataDto.year
     }
 
 
